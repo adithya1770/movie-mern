21d47899daeaf093a667dfc87ed52c713ba26d29
adithya1770 <adithyaps929@gmail.com>
2024-08-10 17:40:26 +0530

    restructured all files

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -11,4 +11,69 @@ getMovieNameById = async (req, res) => {
     }
 };
 
-module.exports = { getMovieNameById };
+getMovieByGenre = async (req, res) => {
+    try {
+        const { genre } = req.params;
+        const movies = await Movies.find({genre});
+        res.status(200).send(movies);
+      } 
+      catch
+      {
+      res.status(400).json({ "message": "Movie Not Found" });
+      }
+}
+
+addMovies = async ( req, res) => { 
+    try{
+        const moviesAdd = await Movies.create(req.body);
+        res.status(200).json(moviesAdd)
+    }
+    catch(err){
+        res.status(400).json({"message" : "Couldn't Add"})
+    }
+}
+
+getMovieById = async (req, res) => {
+    try{
+        const { id } =req.params;
+        const moviesAddId = await Movies.findById(id);
+        res.status(200).json(moviesAddId);
+    }
+    catch(err){
+        res.status(400).json({"message" : err})
+    }
+}
+
+getAllMovies = async (req, res) => {
+    try{
+        const moviesRetriveAll = await Movies.find();
+        res.status(200).json(moviesRetriveAll);
+    }
+    catch(err){
+        res.status(400).json({"message" : "Couldn't Retrive Information"})
+    }
+}
+
+deleteMovie = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const moviesDelete = await Movies.findByIdAndDelete(id);
+        res.status(200).json({"message" : "Item Successfully Deleted!"});
+    }
+    catch(err){
+        res.status(400).json({"message" : "Couldn't Delete Item"})
+    }
+}
+
+updateMovie = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const moviesUpdate = await Movies.findByIdAndUpdate(id, req.body);
+        res.status(200).json({"message" : "Item Successfully Updated!"})
+    }
+    catch(err){
+        res.status(400).json({"message" : "Couldn't Update Item"})
+    }
+}
+
+module.exports = { getMovieNameById, getMovieByGenre, addMovies, getMovieById, getAllMovies, deleteMovie, updateMovie };
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movieController');
 
+// get or retrive routes
+
+router.get('/retrive/:genre', movieController.getMovieByGenre);
 router.get('/retrive/moviename/:id', movieController.getMovieNameById);
+router.get('/retrive/byid/:id', movieController.getMovieById);
+router.get('/retriveall', movieController.getAllMovies);
+
+// other routes 
+
+router.delete('/delete/:id', movieController.deleteMovie);
+router.post('/add', movieController.addMovies);
+router.post('/update/:id', movieController.updateMovie);
 
 module.exports = router;
